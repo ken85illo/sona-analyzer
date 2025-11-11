bed4918e1fcec3b65ef5222472f8c3b6528a01cb
benedict <johnsjbinas@gmail.com>
2025-11-11 23:56:12 +0800

    feat: Converted the cpp output into a js object instead of a string. This commit also allows node to send the json in the console.

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,33 @@
 const textarea_elem = document.getElementById("textarea-element");
 
+const PORT = 8081;
+const URL = `http://localhost:${PORT}/api/lexical-analyzer`;
+
+const lexicalAnalyzer = async (text_JSON) => {
+    try {
+        const rawResponse = await fetch(URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(text_JSON)
+        });
+
+        if (!rawResponse.ok) throw new Error("Server error");
+
+        const lexicalAnalysis = await rawResponse.json();
+        console.log(lexicalAnalysis);
+    } catch (err) {
+        console.error("Fetch error:", err);
+    }
+};
+
 const handleSubmit = () => {
 
     // Provide format of JSON to be sent
-    const textarea_JSON = {
+    const text_JSON = {
         text: textarea_elem.value
     }
 
     console.log("Handle Submit");
 
-    const PORT = 8081;
-    const URL = `http://localhost:${PORT}/api/lexical-analyzer`;
-
-    fetch(URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(textarea_JSON)
-    })
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("output").textContent = data.text
-            console.log("Fetch")
-        });
-
-    // Handle DTO mapping of received JSON of tokens and lexemes
+    lexicalAnalyzer(text_JSON);
 }
