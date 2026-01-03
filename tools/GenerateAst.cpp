33d28b011b80ea94ce7ccea4f80e99274d2c5108
ken85illo <pabillokenneth81@gmail.com>
2026-01-03 19:50:24 +0800

    feat: tool for generating abstract syntax tree

---
+++ tools/GenerateAst.cpp
@@ -0,0 +1,167 @@
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using Str = std::string;
+using StrRef = const Str &;
+using namespace std::literals::string_literals;
+
+Str trim(Str &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+    s.erase(
+        std::find_if(
+            s.rbegin(), s.rend(),
+            [](unsigned char ch) {
+        return !std::isspace(ch);
+    }
+        ).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+std::vector<Str> split(StrRef s, char delim) {
+    std::vector<Str> strList;
+    std::stringstream str(s);
+    Str token;
+
+    while (getline(str, token, delim)) {
+        strList.push_back(token);
+    }
+
+    return strList;
+}
+
+void defineType(std::ofstream &file, StrRef baseName, StrRef className, StrRef fieldList) {
+    file << "class " << className << baseName << " : " << baseName << " {\n";
+    file << "public:\n";
+
+    auto fields = split(fieldList, ',');
+    for (auto &field: fields) {
+        trim(field);
+    }
+
+    file << "    " << className << baseName << "(";
+    for (int i = 0; i < fields.size(); i++) {
+        auto strList = split(fields[i], ' ');
+        auto type = trim(strList[0]);
+        auto name = trim(strList[1]);
+
+        file << "const Ref<" << type << "> " << name;
+
+        if (i < fields.size() - 1) {
+            file << ", ";
+        }
+    }
+    file << ")\n";
+    file << "    " << ": ";
+
+    for (int i = 0; i < fields.size(); i++) {
+        auto strList = split(fields[i], ' ');
+        auto name = trim(strList[1]);
+
+        file << "m_" << name << "(" << name << ")";
+
+        if (i < fields.size() - 1) {
+            file << ", ";
+        }
+    }
+    file << " {}\n\n";
+
+    file << "    std::string accept(Ref<Visitor> visitor) override {\n";
+    file << "        return visitor->visit" << className << baseName << "(*this);\n";
+    file << "    }\n\n";
+
+    file << "private:\n";
+
+    for (auto field: fields) {
+        auto strList = split(field, ' ');
+        auto type = trim(strList[0]);
+        auto name = trim(strList[1]);
+
+        file << "    const Ref<" << type << "> m_" << name << ";\n";
+    }
+
+    file << "};\n\n";
+}
+
+void defineVisitor(std::ofstream &file, StrRef baseName, const std::vector<Str> &types) {
+    file << "    class Visitor {\n";
+    file << "    public:\n";
+
+    for (auto type: types) {
+        auto typeName = trim(split(type, ':')[0]);
+        auto lowerBaseName = baseName;
+        std::transform(lowerBaseName.begin(), lowerBaseName.end(), lowerBaseName.begin(), [](unsigned char c) {
+            return std::tolower(c);
+        });
+
+        file << "        virtual std::string visit" << typeName << baseName << "(";
+        file << typeName << baseName << " " << lowerBaseName << ") = 0;\n";
+    }
+
+    file << "    };\n\n";
+}
+
+void defineAst(StrRef outputDir, StrRef baseName, const std::vector<Str> &types) {
+    std::filesystem::path path = outputDir;
+    path /= (baseName + ".h"s);
+
+    std::ofstream file(path);
+
+    file << "#include \"Ref.h\"" << "\n";
+    file << "#include \"Token.h\"" << "\n\n";
+
+    for (auto type: types) {
+        auto typeName = trim(split(type, ':')[0]);
+        file << "class " << typeName << baseName << ";\n";
+    }
+    file << "\n";
+
+    file << "class " << baseName << " {\n";
+    file << "public:\n";
+    defineVisitor(file, baseName, types);
+    file << "protected:\n";
+    file << "    virtual std::string accept(Ref<Visitor> visitor) = 0;\n";
+    file << "};\n\n";
+
+    // AST Classes
+    for (auto type: types) {
+        auto strList = split(type, ':');
+        Str className = trim(strList[0]);
+        Str fields = trim(strList[1]);
+        defineType(file, baseName, className, fields);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: generate_ast <output_directory>\n";
+        exit(1);
+    }
+
+    Str outputDir = argv[1];
+
+    std::vector<Str> strList;
+    std::ifstream file("grammar.txt");
+    Str str;
+
+    Str baseName;
+    std::getline(file, baseName); // first line
+    trim(baseName);
+    std::cout << "Base Name: " << baseName << "\n";
+
+    int i = 0;
+    while (std::getline(file, str)) {
+        strList.push_back(str);
+        std::cout << "[" << i++ << "]: " << str << "\n";
+    }
+
+    defineAst(outputDir, baseName, strList);
+}
