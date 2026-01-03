bf97ba8fd478da2c719182da5460a98aaa72be35
ken85illo <pabillokenneth81@gmail.com>
2026-01-04 03:49:15 +0800

    fix: validator marking * as unknown token before - and + unary

--- src/TokenUtils.h
+++ src/TokenUtils.h
@@ -18,7 +18,8 @@ inline bool isAlphaNumeric(char c) {
 
 inline bool isValue(TokenType type) {
     return type == IDENTIFIER || type == INT_LITERAL || type == FLT_LITERAL || type == POST_INCRMNT_OP ||
-           type == POST_DECRMNT_OP || type == PRE_DECRMNT_OP || type == POST_DECRMNT_OP || type == STR_LITERAL;
+           type == POST_DECRMNT_OP || type == PRE_DECRMNT_OP || type == POST_DECRMNT_OP || type == STR_LITERAL ||
+           type == POSITIVE_OP || type == NEGATIVE_OP;
 }
 
 inline bool isUserType(TokenType type) {
