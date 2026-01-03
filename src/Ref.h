f4d29dd07586060986e6ec248e6b5a658bf95f39
ken85illo <pabillokenneth81@gmail.com>
2026-01-03 19:49:57 +0800

    feat: ref header file

---
+++ src/Ref.h
@@ -0,0 +1,8 @@
+
+template <typename T>
+using Ref = std::shared_ptr<T>;
+
+template <typename T, typename... Args>
+std::shared_ptr<T> MakeRef(Args &&...args) {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
